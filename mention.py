9955119340c5e0e00edd9fb2d6cfa3ed0f3f5faa
AnjulaAbey <abeyweeraanjula@gmail.com>
2024-07-18 17:30:18 +0530

    working for small and large groups

--- mention.py
+++ mention.py
@@ -6,6 +6,10 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
+
+# Inputs
+n= 10 # Number of times to press the down arrow key to load more contacts
+group = "cse track xi" # Group name
 # Path to your geckodriver
 driver_path = '/snap/bin/geckodriver'
 service = Service(executable_path=driver_path)
@@ -35,11 +39,10 @@ try:
         EC.element_to_be_clickable((By.XPATH, '//div[@contenteditable="true"][@data-tab="3"]'))
     )
     
-    group_name= "CsE tRaCk Xi" #Group name
-    for i in range(0,len(group_name)):
-        search_box.send_keys(group_name[i])
+    group_name = group 
+    for char in group_name:
+        search_box.send_keys(char)
 
-    
     # Wait for the search box to update with the full query
     time.sleep(5)  # Adjust as needed
     
@@ -64,24 +67,81 @@ try:
         EC.visibility_of_element_located((By.XPATH, '//div[@title="Group info"]'))
     )
     print("Group Info Loaded")
-    time.sleep(20)
-
-    # Retrieve all group members
+    time.sleep(60)
     members = []
-    member_elements = WebDriverWait(driver, 10).until(
-        EC.presence_of_all_elements_located((By.XPATH, '//div[@role="gridcell"]//span[@dir="auto"]'))
-    )
-    for element in member_elements:
-            members.append(element.text)
-    members.pop(0)  # Remove the first element which is the group name
-    members.pop(0)  # Remove the second element which is my contact
+    # Check if the specific element is visible
+    try:
+        special_element = WebDriverWait(driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, '//div[@role="button"][@tabindex="0"][@data-ignore-capture="any"]'))
+        )
+        if n<15:
+            print("group members are less than 15")
+            member_elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//div[@role="gridcell"]//span[@dir="auto"]'))
+            )
+                # Retrieve all group members
+            for element in member_elements:
+                members.append(element.text)
+            members.pop(0)
+            members.remove('You')
+            # Go back to the group chat
+            driver.find_element(By.XPATH, '//div[@aria-label="Close"]').click()
+            print("Back to group chat")
+        elif special_element:
+            print("Special element is visible.")
+            special_element.click()
+            time.sleep(10)
+            # Simulate pressing the down arrow key to load more contacts
+            body = driver.find_element(By.TAG_NAME, 'body')
+            for i in range(n):  
+                body.send_keys(Keys.ARROW_DOWN)
+                time.sleep(0.2) 
+                if(i%10 == 0):
+                    member_elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//div[@role="gridcell"]//span[@dir="auto"]'))
+            )
+                # Retrieve all group members
+                    for element in member_elements:
+                        members.append(element.text)
+                # Go back to the group chat
+            driver.find_element(By.XPATH, '//div[@aria-label="Close"]').click()
+            time.sleep(5)
+            driver.find_element(By.XPATH, '//div[@aria-label="Close"]').click()
+            print("Back to group chat")
+
+        else:
+            print("Special element is not visible.")
+            member_elements = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, '//div[@role="gridcell"]//span[@dir="auto"]'))
+            )
+                # Retrieve all group members
+            for element in member_elements:
+                members.append(element.text)
+    except Exception as e:
+        print("Special element is not present.")
+        member_elements = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//div[@role="gridcell"]//span[@dir="auto"]'))
+        )
+    
+
+    members = list(set(members))
+    print(len(members))
+    # Filter out members with \u202f and modify phone numbers
+    filtered_members = []
+    for member in members:
+        if '\u202f' not in member:
+            if member.startswith('+'):
+                filtered_members.append(member[:-1])
+            else:
+                filtered_members.append(member[:-1])
+    
     # Print filtered members list
     print("Filtered Members List:", end=' ')
-    print(members)
+    print(filtered_members)
     time.sleep(5)
-    # Go back to the group chat
-    driver.find_element(By.XPATH, '//div[@aria-label="Close"]').click()
-    print("Back to group chat")
+    
+
+    
     # Wait for the chat to load
     WebDriverWait(driver, 30).until(
         EC.visibility_of_element_located((By.XPATH, '//div[@contenteditable="true"][@data-lexical-editor="true"]'))
@@ -92,12 +152,13 @@ try:
     chat_box.click()
 
     # Typing the mentions
-    for member in members:
+    for member in filtered_members:
         chat_box.send_keys('@')
-        for i in range(0,len(member)):
-            chat_box.send_keys(member[i])
+        for char in member:
+            chat_box.send_keys(char)
         time.sleep(1)
         chat_box.send_keys(Keys.TAB)
+        chat_box.send_keys(Keys.SPACE)
 
     # Sending the message
     # chat_box.send_keys(Keys.ENTER)
@@ -110,4 +171,4 @@ except Exception as e:
 finally:
     # Quit the driver
     print("Quitting the driver...")
-    # driver.quit()
+    driver.quit()
